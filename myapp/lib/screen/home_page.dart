5cbd0bbb8236898088aa2929f214baab20e54134
PhengMenghour <phengmenghour@gmail.com>
2024-07-25 22:00:01 +0700

    Progressing

--- myapp/lib/screen/home_page.dart
+++ myapp/lib/screen/home_page.dart
@@ -5,6 +5,27 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.pink,
+      leading: IconButton(icon: Icon(Icons.home), onPressed: (){},),
+      title: Text("Hello Flutter App"),),
+
+      body: Container(alignment: Alignment.center,
+      child: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFzhsnlVVQ1PCnMjdoBqFtQJILUpyPKVEGqg&s"),),
+
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            IconButton(icon: Icon(Icons.home),
+            onPressed: (){},),
+            IconButton(icon: Icon(Icons.play_circle_fill),
+            onPressed: (){},),
+            IconButton(icon: Icon(Icons.menu),
+            onPressed: (){},)
+          ],
+        ),
+      ),
+    );
   }
 }
